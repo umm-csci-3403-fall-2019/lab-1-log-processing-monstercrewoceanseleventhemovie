70508393a06ef7b8f115fafe9ae89c72704053bc
leahy089 <leahy089@morris.umn.edu>
2019-09-10 11:42:21 -0500

    Alter the two username scripts to almost work
    We have done what we need to do in essence, but with some major
    tweaking of various things.
    
    1) `process_client.logs.sh` needs to take only the single temp
    directory as an argument, instead fo us feeding each machine in
    individually.
    
    2) According to doing a diff on the test file for the two tested
    computers, we aren't getting the right number of results for some
    reason.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,4 +2,5 @@
 
 dir=$1
 
-sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' < ../log_files/zeus_secure/failed_login_data.txt | sort | uniq -c > test.txt
+sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt | sort | uniq -c | awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > $dir/username_dist.html
+
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
-
-#directory=$1
-#$directory/var/log
-#current_direct=$(pwd)
-
 cd "$1"
 
-#files=$(find $current_direct -type f -printf "%f\n")
+sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]:([a-zA-Z ]+) for (invalid user )?([a-zA-Z0-9 _-]+) from ([0-9.]+) (.+)/\1 \7 \8/p' -r var/log/* > failed_login_data.txt
 
-#for i in $files
-#do
-	sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]:([a-zA-Z ]+) for (invalid user )?([a-zA-Z0-9 _-]+) from ([0-9.]+) (.+)/\1 \7 \8/p' -r var/log/* > failed_login_data.txt
-#done
 
