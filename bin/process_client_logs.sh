41ec2644224e8f232e4b2a6af9e4c51bacb37316
leahy089 <leahy089@morris.umn.edu>
2019-09-09 19:54:24 -0500

    finish the process_client_logs.sh code
    regex completed for the code. failed_login_data.txt is created in each computer's respective folder now

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-directory=$1
-$directory/var/log
-current_direct=$(pwd)
 
-files=$(find $current_direct -type f -printf "%f\n")
+#directory=$1
+#$directory/var/log
+#current_direct=$(pwd)
 
-for i in $files
-do
-	sed -E 's/\([a-zA-Z0-9]+):/\1\n/' < $i >> failed_login_data.txt
-done
+cd "$1"
+
+#files=$(find $current_direct -type f -printf "%f\n")
+
+#for i in $files
+#do
+	sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]:([a-zA-Z ]+) for (invalid user )?([a-zA-Z]+) from ([0-9.]+) (.+)/\1 \7 \8/p' -r var/log/* > failed_login_data.txt
+#done
 
