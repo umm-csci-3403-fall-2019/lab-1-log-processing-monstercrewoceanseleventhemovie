45c7a4ca3a9a578288f3519de850857dc446a73f
leahy089 <leahy089@morris.umn.edu>
2019-09-10 17:26:32 -0500

    Add the wrap_contents.sh file in, fix the process_client_logs.sh regex

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,5 +2,5 @@
 
 dir=$1
 
-sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt | sort | uniq -c | awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);"}' > $dir/username_dist.html
-
+neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt | sort | uniq -c | awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}')
+#source ./bin/wrap_contents.sh $neat username_dist this
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,6 +2,7 @@
 
 cd "$1"
 
-sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]:([a-zA-Z ]+) for (invalid user )?([a-zA-Z0-9 _-]+) from ([0-9.]+) (.+)/\1 \7 \8/p' -r var/log/* > failed_login_data.txt
+sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]: Failed password for (invalid user )?([a-zA-Z0-9 _-]+) from ([0-9.]+) (.+)/\1 \6 \7/p' -r var/log/* > failed_login_data.txt
+
 
 
