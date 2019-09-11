2a418dd769ba963e509901ea6263af7e111bd57f
michael-small <small203@morris.umn.edu>
2019-09-11 16:02:25 -0500

    Clean up sed formatting, add comments to specify script arguments

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-dir=$1
+dir=$1 ##Takes 1 arg: name of a directory
 
-neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt | sort | uniq -c | awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}')
-#source ./bin/wrap_contents.sh $neat username_dist this
+neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt 
+	| sort 
+	| uniq -c 
+	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}')
+
+#`wrap.contents.sh` takes 3 args: 
+	# 1) filename of "contents" to be wrapped
+	# 2) name to specify desired header/footer
+	# 3) filename of result
+
+#./bin/wrap_contents.sh $neat username_dist this
