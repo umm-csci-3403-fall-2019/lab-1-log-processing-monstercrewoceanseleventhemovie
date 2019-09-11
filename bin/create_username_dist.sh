f44177c01b4b0f8d7b23402ba17039595045ff5d
michael-small <small203@morris.umn.edu>
2019-09-11 16:11:51 -0500

    Add `\` to allow newlines
    Previous commit tried to break up lines, but the `\`
    char is needed at the end of each line.

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+pwd
 dir=$1 ##Takes 1 arg: name of a directory
 
-neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt 
-	| sort 
-	| uniq -c 
-	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}')
+neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt \
+	| sort \
+	| uniq -c \
+	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}') \
 
 #`wrap.contents.sh` takes 3 args: 
 	# 1) filename of "contents" to be wrapped
