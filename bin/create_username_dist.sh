755dc46397359daac109e83c5fc5b008862be5af
michael-small <small203@morris.umn.edu>
2019-09-11 19:15:32 -0500

    Add newline \ marks, correct `wrap_contents.sh` args

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-pwd
-dir=$1 ##Takes 1 arg: name of a directory
+dir=$1 #Takes 1 arg: name of a directory
 
-neat=$(sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt \
+sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt \
 	| sort \
 	| uniq -c \
-	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}') \
+	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}' > $dir/username_dist.txt  
 
 #`wrap.contents.sh` takes 3 args: 
 	# 1) filename of "contents" to be wrapped
 	# 2) name to specify desired header/footer
 	# 3) filename of result
 
-#./bin/wrap_contents.sh $neat username_dist this
+./bin/wrap_contents.sh $dir/username_dist.txt html_components/username_dist $dir/username_dist.html
+
+rm -rf $dir/username_dist.txt #file no longer used
