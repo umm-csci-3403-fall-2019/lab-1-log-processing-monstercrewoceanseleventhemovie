0b486d3398c585b7fc0d9fc106c04986a5ace31a
michael-small <small203@morris.umn.edu>
2019-09-11 19:20:02 -0500

    Add clarifying comment to `process_client_logs.sh`

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,6 +2,7 @@
 
 cd "$1"
 
+# Extracts the relevant data under the specified machine name's var/log file group into a text file
 sed -En 's/(^.{0,9}):([0-9]+):([a-zA-Z0-9 _]+)\[([0-9]+)\]: Failed password for (invalid user )?([a-zA-Z0-9 _-]+) from ([0-9.]+) (.+)/\1 \6 \7/p' -r var/log/* > failed_login_data.txt
 
 
