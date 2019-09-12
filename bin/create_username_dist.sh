27fd5f2943b62e5923eb2bd014fd4170f9def3e2
michael-small <small203@morris.umn.edu>
2019-09-11 19:23:26 -0500

    Run shellcheck on the two scripts
    Most of the errors were about wrapping variables in quotes. One error
    was about adding a `|| exit` to a `cd`

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -5,13 +5,13 @@ dir=$1 #Takes 1 arg: name of a directory
 sed -E 's/(^.{0,9}) ([a-zA-Z0-9 _-]+) ([0-9.]+)/\2/' -r log_files/*/failed_login_data.txt \
 	| sort \
 	| uniq -c \
-	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}' > $dir/username_dist.txt  
+	| awk '/([ ]+)([0-9]+) ([a-zA-Z0-9_-]+)/{print "data.addRow([\x27" $2 "\x27, " $1 "]);\n"}' > "$dir"/username_dist.txt  
 
 #`wrap.contents.sh` takes 3 args: 
 	# 1) filename of "contents" to be wrapped
 	# 2) name to specify desired header/footer
 	# 3) filename of result
 
-./bin/wrap_contents.sh $dir/username_dist.txt html_components/username_dist $dir/username_dist.html
+./bin/wrap_contents.sh "$dir"/username_dist.txt html_components/username_dist "$dir"/username_dist.html
 
-rm -rf $dir/username_dist.txt #file no longer used
+rm -rf "$dir"/username_dist.txt #file no longer used
